c449060149d2ffa76b76df6872a3f1c71f7f4c96
Hery <aeonzamaek@gmail.com>
2013-04-09 16:08:21 -0400

    .gitignore

--- js/splashcloud.js
+++ js/splashcloud.js
@@ -1,4 +1,9 @@
-$('span.vertical').each(function() {
+$('span.vertical').each(function(index) {
+  var freq = this.dataset.freq;
+  $(this).css('font-size', freq*2);
+});
+
+$('span.horizontal').each(function(index) {
   var freq = this.dataset.freq;
   $(this).css('font-size', freq*2);
 });
